683e6b8c365318feefcb1f4b5dce540d0c82c307
jhareeshkumar <jhareesh30@gmail.com>
2023-12-21 13:22:23 +0530

    added create course & add lesson pages without business logic

--- src/main/java/com/learnSphere/controller/NavController.java
+++ src/main/java/com/learnSphere/controller/NavController.java
@@ -2,8 +2,6 @@ package com.learnSphere.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class NavController {
@@ -11,21 +9,30 @@ public class NavController {
 	public String index() {
 		return "index";
 	}
-	
+
 	@GetMapping("/register")
 	public String register() {
 		return "register";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
 
-	
 	@GetMapping("/home")
-	public String  home() {
+	public String home() {
 		return "home";
 	}
-	
+
+	@GetMapping("/createCourse")
+	public String createCourse() {
+		return "createCourse";
+	}
+
+	@GetMapping("/addLesson")
+	public String addLesson() {
+		return "addLesson";
+	}
+
 }
